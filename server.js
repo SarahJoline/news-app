1c236391c291315ca27c0ee270f8455f468dda4f
Sarah Naas <sjnaas@gmail.com>
2020-06-30 11:03:48 -0700

    testing

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require("path");
 const mongoose = require("mongoose");
 const connection = mongoose.connection;
 const MONGO_URI =
@@ -26,6 +27,13 @@ app.use("/api", apiRoutes);
 
 const htmlRoutes = require("./routes/htmlRoutes");
 app.use("/", htmlRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`listening at: http://localhost:${PORT}`);
